a5cd75a9a0295f5cf2a70cb9d69dbc8d9999ebb7
Shishir Poudel <66148659+Shishir3D@users.noreply.github.com>
2024-08-26 18:05:51 +0545

    Time: 44 ms (68.21%) | Memory: 17.1 MB (61.94%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,7 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for each in matrix:
-            if target in each:
-                return True
+        top = 0
+        bottom = len(matrix) - 1
+
+        # find the row where the number resides
+        while top <= bottom:
+            middle = (top + bottom) // 2
+            mm = matrix[middle]
+            if target >= mm[0] and target <= mm[-1]:
+                break
+            elif target > mm[-1]:
+                top = middle + 1
+            elif target < mm[0]:
+                bottom = middle - 1
+
+        if target in mm:
+            return True
         return False
+
+        # l = 0
+        # r = len(mm) -1
+
+        # while l <= r:
+
+
+
+        
         
