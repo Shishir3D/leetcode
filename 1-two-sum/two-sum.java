2f6c0e1373c9ad9f7e649f562333805654ead1f7
Shishir Poudel <66148659+Shishir3D@users.noreply.github.com>
2024-09-07 21:10:02 +0545

    Time: 2 ms (98.43%) | Memory: 45.5 MB (5.66%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,22 +3,20 @@ class Solution {
         // put elements as key and its indices as values in a hash map
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] result = new int[2];
-
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
+        int diff;
 
         for (int j = 0; j < nums.length; j++) {
-            
-            if (map.containsKey(target - nums[j]) &&  
-                map.get( target - nums[j] ) != j) {
+            diff = target - nums[j];
+            if (map.containsKey(diff) &&  
+                map.get( diff ) != j) {
                 result[0] = j;
-                result[1] = map.get( target - nums[j] );
+                result[1] = map.get( diff );
                 return result;
+            } else {                
+                map.put(nums[j], j);
             }
         }
 
         return result;
-
     }
 }
