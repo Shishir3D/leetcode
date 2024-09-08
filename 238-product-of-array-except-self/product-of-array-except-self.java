9bf9ac294ab618d9a0cd131d1979be8b275d3b61
Shishir Poudel <66148659+Shishir3D@users.noreply.github.com>
2024-09-09 04:25:55 +0545

    Time: 3 ms (23.47%) | Memory: 55.2 MB (81.06%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,31 +1,20 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        // create a suffix and prefix array
-        // each index in suffix array will contain the product 
-        // of all the elements that are to the left of the suffix (not inclusive)
-        // elements in prefix is the same as suffix but have product of elements to the right (not inc)
-        // multiply suffix and prefix at every index to get the answer
-
-        ArrayList<Integer> suffix = new ArrayList<>();
-        ArrayList<Integer> prefix = new ArrayList<>();
+    public int[] productExceptSelf(int[] nums) 
+    {
         int n = nums.length;
         int [] result = new int[n];
+        int curr = 1;
 
-        for (int i = 0; i < n; i++) {
-            suffix.add(1);
-            prefix.add(1);
+        Arrays.fill(result, 1);
+
+        for(int i = 0; i < n; i++) {
+            result[i] *= curr;
+            curr *= nums[i];
         }
-
-        for (int j = 1; j < n; j++) {
-            suffix.set(j, suffix.get(j-1) * nums[j-1]);
-        } 
-
-        for (int k = n-2; k > -1; k--) {
-            prefix.set(k, prefix.get(k+1) * nums[k+1]);
-        }
-        
-        for (int l = 0; l < n; l++) {
-            result[l] = suffix.get(l) * prefix.get(l);
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            result[i] *= curr;
+            curr *= nums[i];
         }
 
         return result;
