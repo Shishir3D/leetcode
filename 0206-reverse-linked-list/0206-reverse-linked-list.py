be076343a6882d2dedc2eb0e28abc43b7a69d90d
Shishir Poudel <66148659+Shishir3D@users.noreply.github.com>
2025-02-23 23:14:37 +0545

    Time: 0 ms (100%), Space: 18.6 MB (53.93%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,13 +5,14 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = head
-        prev = None
+        prev_pointer = None
+        curr_pointer = head
 
-        while curr != None:
-            nextt = curr.next
-            curr.next  = prev
-            prev = curr
-            curr = nextt
-        
-        return prev
+        while curr_pointer is not None:
+            next_pointer = curr_pointer.next #stores '2' node's link in the first iteration
+            curr_pointer.next = prev_pointer #makes '1' as the last node by making the '1' node point None
+            prev_pointer = curr_pointer
+            curr_pointer = next_pointer
+
+        return prev_pointer            
+
