2c0075dedf7703fd6f2a4638f25d7fa63766a503
Shishir Poudel <66148659+Shishir3D@users.noreply.github.com>
2025-02-23 23:53:18 +0545

    Time: 0 ms (100%), Space: 19 MB (22.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -1,10 +1,31 @@
-int comp(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
-
 bool containsDuplicate(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), comp);
+   
+    int min = nums[0];
+    int max = nums[0];
 
-    for (int i = 0; i < numsSize - 1; i++)
-        if (nums[i] == nums[i + 1])
+    for(int i = 0; i < numsSize; i++) {
+        if(min > nums[i]) min = nums[i];
+        if(max < nums[i]) max = nums[i];
+    }
+
+    printf("max: %d\n", max);
+    printf("min: %d\n", min);
+
+    int capacity = max - min + 1;
+
+    printf("capacity %d\n", capacity);
+    int *hashmap = calloc(capacity, sizeof(int));
+    
+    for(int i = 0; i < numsSize; i++){
+        if(hashmap[nums[i]-min] >= 1) {
+            free(hashmap);
             return true;
+        }
+
+        hashmap[nums[i]-min]++;
+        
+    }
+
     return false;
+
 }
